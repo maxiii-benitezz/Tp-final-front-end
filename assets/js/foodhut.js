8f5fe59de304f6ca179d5748dc09c4ff2b29f246
maxiii-benitezz <maxiiibenitezz91218@hotmail.com>
2024-12-09 16:23:45 -0300

    Add files via upload

--- assets/js/foodhut.js
+++ assets/js/foodhut.js
@@ -45,12 +45,80 @@ function initMap() {
     });
  }
 
- function claro() { document.body.style.backgroundColor="white";
+ function claro() { document.getElementById("claro").style.display = "none";
+    document.getElementById("oscuro").style.display = "block";
+    document.body.style.backgroundColor="#c4b5b5"; 
+    const elementos = document.querySelectorAll('h2, h3, p, a, h6, h4');
+    elementos.forEach(function(elemento) {
+        elemento.style.color = "#d4002c";
+        document.getElementById("rojo").style.color="#d4002c"
+        document.getElementById("carta1").style.backgroundColor="white"
+        document.getElementById("carta2").style.backgroundColor="white"
+        document.getElementById("carta3").style.backgroundColor="white"
+         document.getElementById("pills-home-tab").style.backgroundColor="white"
+         document.getElementById("rojito1").style.color="#e0758b"
+         document.getElementById("rojito2").style.color="#e0758b"
+         document.getElementById("rojito3").style.color="#e0758b"
+         document.getElementById("rounded-btn").style.color="white"
+         document.getElementById("sectionn").style.backgroundColor="#c4b5b5"
+         document.getElementById("footer").style.backgroundColor="#c4b5b5"
+         document.getElementById("nav").style.backgroundColor="#c4b5b5"
+          document.getElementById("paraiso").style.color="white"
+        
+
+         
+    });
+
+ }
+function oscuro() { 
+    document.getElementById("claro").style.display = "block";
+    document.getElementById("oscuro").style.display = "none";
+    document.body.style.backgroundColor="#343a40"; 
+    const elementos = document.querySelectorAll('h2, h3, p, a, h6, h4');
+    elementos.forEach(function(elemento) {
+        elemento.style.color = "#f8f9fa";
+        document.getElementById("rojo").style.color="#f8f9fa"
+        document.getElementById("carta1").style.backgroundColor="#ff214f"
+        document.getElementById("carta2").style.backgroundColor="#ff214f"
+        document.getElementById("carta3").style.backgroundColor="#ff214f"
+         document.getElementById("pills-home-tab").style.backgroundColor=""
+         document.getElementById("rojito1").style.color="white"
+         document.getElementById("rojito2").style.color="white"
+         document.getElementById("rojito3").style.color="white"
+         document.getElementById("blog").style.color="#ff214f"
+         document.getElementById("sectionn").style.backgroundColor="#343a40"
+         document.getElementById("nav").style.backgroundColor="#343a40"
+         document.getElementById("footer").style.backgroundColor="#343a40"
+         document.getElementById("book-table").style.backgroundColor="#343a40"
+    
+        
+       
+        
+
+         
+    });
 
  }
 
-function persona1() {alert("Nombre: Máximo")}
 
-function persona2() {alert("Nombre: Máximo")}
 
-function persona3() {alert("Nombre: Máximo")}
+
+
+function persona1() {alert ("Nombre: Máximo Benítez\n" +
+        "Correo: maximo.benitez@webdesignpro.com\n" +
+        "Rol: Diseñador Web\n" +
+        "Responsabilidad: Coordinar el diseño gráfico y la estructura visual del sitio web, asegurando que la estética del proyecto esté alineada con los objetivos del cliente."
+    );
+}
+function persona2() {alert("Nombre: Emiliano Benítez\n" +
+        "Correo: emiliano.benitez@webguidesolutions.com\n" +
+        "Rol: Guiador de Visitas\n" +
+        "Responsabilidad: Facilitar la interacción de los usuarios con el sitio web, proporcionando orientación clara y optimizando la experiencia de navegación."
+    );
+}
+function persona3() {alert("Nombre: Lorenzo Benítez\n" +
+        "Correo: lorenzo.benitez@financeplanner.com\n" +
+        "Rol: Asesor Financiero\n" +
+        "Responsabilidad: Supervisar la estrategia financiera del proyecto, asegurando una asignación eficiente de los recursos y la viabilidad económica a largo plazo."
+    );
+}
